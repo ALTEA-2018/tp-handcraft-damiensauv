34b5adc23f97cbd4f7f4d3dfe5a01dfbb2280693
SAUVALLE Damien <damien.sauvalle.etu@univ-lille.fr>
2019-01-14 10:44:51 +0100

    up : update first servlet

--- src/main/java/FirstServlet.java
+++ src/main/java/FirstServlet.java
@@ -1,16 +1,20 @@
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
+@WebServlet(urlPatterns = "/*",
+        loadOnStartup = 1)
 public class FirstServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
-        var writer = resp.getWriter();
+        PrintWriter writer = resp.getWriter();
         writer.println("Hello !");
     }
 
