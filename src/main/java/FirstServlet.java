11c3b307869fff892286b6f69fc2773c82e73c06
SAUVALLE Damien <damien.sauvalle.etu@univ-lille.fr>
2019-01-14 10:42:32 +0100

    feat : first servlet

--- src/main/java/FirstServlet.java
+++ src/main/java/FirstServlet.java
@@ -1,2 +1,23 @@
-public class FirstServlet {
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class FirstServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        var writer = resp.getWriter();
+        writer.println("Hello !");
+    }
+
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        super.init(config);
+
+        System.out.println("Initialisation de la servlet");
+    }
 }
