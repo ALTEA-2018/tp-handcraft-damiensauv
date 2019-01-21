6c7adcee888ffe263ea10b3e00d0a14a61e4187a
SAUVALLE Damien <damien.sauvalle.etu@univ-lille.fr>
2019-01-21 09:46:10 +0100

    update dep

--- src/test/java/JacksonDatabindTest.java
+++ src/test/java/JacksonDatabindTest.java
@@ -1,4 +1,35 @@
-package PACKAGE_NAME;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
 
-public class JacksonDatabindTest {
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class JacksonDatabindTest {
+
+    public static class Car {
+        public String color;
+        public String brand;
+    }
+
+    @Test
+    void testWriteJson() throws JsonProcessingException {
+        var objectMapper = new ObjectMapper();
+        var car = new Car();
+        car.color = "yellow";
+        car.brand = "renault";
+        var json = objectMapper.writeValueAsString(car);
+        assertEquals("{\"color\":\"yellow\",\"brand\":\"renault\"}", json);
+    }
+
+    @Test
+    void testReadJson() throws IOException {
+        var objectMapper = new ObjectMapper();
+        var json = "{ \"color\" : \"black\", \"brand\" : \"opel\" }";
+        var car = objectMapper.readValue(json, Car.class);
+        assertEquals("black", car.color);
+        assertEquals("opel", car.brand);
+    }
+
 }
